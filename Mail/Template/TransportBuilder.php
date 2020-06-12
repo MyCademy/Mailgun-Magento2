99065a2004fd39dcda7ff9d468b8a8fbf6336c33
N.L.P. van der Meij <n.vandermeij@mycademy.com>
2020-06-12 14:07:14 +0200

    bugfix: Fixed problem with setting private properties on parent class

--- Mail/Template/TransportBuilder.php
+++ Mail/Template/TransportBuilder.php
@@ -9,10 +9,19 @@ declare(strict_types=1);
 
 namespace Bogardo\Mailgun\Mail\Template;
 
+use Magento\Framework\Mail\AddressConverter;
+use Magento\Framework\Mail\EmailMessageInterfaceFactory;
+use Magento\Framework\Mail\MessageInterface;
+use Magento\Framework\Mail\MessageInterfaceFactory;
 use Magento\Framework\Mail\MimeMessageInterfaceFactory;
 use Magento\Framework\Mail\MimePartInterfaceFactory;
+use Magento\Framework\Mail\Template\FactoryInterface;
+use Magento\Framework\Mail\Template\SenderResolverInterface;
 use Magento\Framework\Mail\Template\TransportBuilder as BaseTransportBuilder;
 use Magento\Framework\App\TemplateTypesInterface;
+use Magento\Framework\Mail\TransportInterface;
+use Magento\Framework\Mail\TransportInterfaceFactory;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Phrase;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Mail\MimeInterface;
@@ -27,6 +36,86 @@ use Magento\Framework\Mail\MimeInterface;
 class TransportBuilder extends BaseTransportBuilder
 {
     /**
+     * Template Identifier
+     *
+     * @var string
+     */
+    protected $templateIdentifier;
+
+    /**
+     * Template Model
+     *
+     * @var string
+     */
+    protected $templateModel;
+
+    /**
+     * Template Variables
+     *
+     * @var array
+     */
+    protected $templateVars;
+
+    /**
+     * Template Options
+     *
+     * @var array
+     */
+    protected $templateOptions;
+
+    /**
+     * Mail Transport
+     *
+     * @var TransportInterface
+     */
+    protected $transport;
+
+    /**
+     * Template Factory
+     *
+     * @var FactoryInterface
+     */
+    protected $templateFactory;
+
+    /**
+     * Object Manager
+     *
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * Message
+     *
+     * @var MessageInterface
+     */
+    protected $message;
+
+    /**
+     * Sender resolver
+     *
+     * @var SenderResolverInterface
+     */
+    protected $_senderResolver;
+
+    /**
+     * @var TransportInterfaceFactory
+     */
+    protected $mailTransportFactory;
+
+    /**
+     * Param that used for storing all message data until it will be used
+     *
+     * @var array
+     */
+    protected $messageData = [];
+
+    /**
+     * @var EmailMessageInterfaceFactory
+     */
+    protected $emailMessageInterfaceFactory;
+
+    /**
      * @var MimeMessageInterfaceFactory
      */
     protected $mimeMessageInterfaceFactory;
@@ -35,6 +124,53 @@ class TransportBuilder extends BaseTransportBuilder
      * @var MimePartInterfaceFactory
      */
     protected $mimePartInterfaceFactory;
+
+    /**
+     * @var AddressConverter|null
+     */
+    protected $addressConverter;
+
+    /**
+     * TransportBuilder constructor
+     *
+     * @param FactoryInterface $templateFactory
+     * @param MessageInterface $message
+     * @param SenderResolverInterface $senderResolver
+     * @param ObjectManagerInterface $objectManager
+     * @param TransportInterfaceFactory $mailTransportFactory
+     * @param MessageInterfaceFactory|null $messageFactory
+     * @param EmailMessageInterfaceFactory|null $emailMessageInterfaceFactory
+     * @param MimeMessageInterfaceFactory|null $mimeMessageInterfaceFactory
+     * @param MimePartInterfaceFactory|null $mimePartInterfaceFactory
+     * @param addressConverter|null $addressConverter
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        FactoryInterface $templateFactory,
+        MessageInterface $message,
+        SenderResolverInterface $senderResolver,
+        ObjectManagerInterface $objectManager,
+        TransportInterfaceFactory $mailTransportFactory,
+        MessageInterfaceFactory $messageFactory = null,
+        EmailMessageInterfaceFactory $emailMessageInterfaceFactory = null,
+        MimeMessageInterfaceFactory $mimeMessageInterfaceFactory = null,
+        MimePartInterfaceFactory $mimePartInterfaceFactory = null,
+        AddressConverter $addressConverter = null
+    ) {
+        $this->templateFactory = $templateFactory;
+        $this->objectManager = $objectManager;
+        $this->_senderResolver = $senderResolver;
+        $this->mailTransportFactory = $mailTransportFactory;
+        $this->emailMessageInterfaceFactory = $emailMessageInterfaceFactory ?: $this->objectManager
+            ->get(EmailMessageInterfaceFactory::class);
+        $this->mimeMessageInterfaceFactory = $mimeMessageInterfaceFactory ?: $this->objectManager
+            ->get(MimeMessageInterfaceFactory::class);
+        $this->mimePartInterfaceFactory = $mimePartInterfaceFactory ?: $this->objectManager
+            ->get(MimePartInterfaceFactory::class);
+        $this->addressConverter = $addressConverter ?: $this->objectManager
+            ->get(AddressConverter::class);
+    }
 
     /**
      * Prepare message.
